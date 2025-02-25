bb1054a5f7c0c1650f3acfaa70fdee4c2535bbb0
AmilMoskalenko <amil.sagyn99@gmail.com>
2025-02-25 18:49:06 +0600

    - Added UI Manager, background, buttons

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] private Button _peachButton;
+    [SerializeField] private Button _lavandaButton;
+    [SerializeField] private Button _yellowButton;
+    [SerializeField] private Button _mintButton;
+
+    private void Start()
+    {
+        //_peachButton.onClick.AddListener(() => );
+
+    }
+}
