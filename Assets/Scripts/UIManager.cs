7cb7f88b8d1c2a6a67652d7246f8260fc7b9ea68
AmilMoskalenko <amil.sagyn99@gmail.com>
2025-02-26 12:35:05 +0600

    - Added timer logic, fail screen

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,6 +1,8 @@
+using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
@@ -18,25 +20,42 @@ public class UIManager : MonoBehaviour
     [SerializeField] private GameObject _pauseScreen;
     [SerializeField] private Button _continueButton;
     [SerializeField] private Button _leaveGameButton;
+    [SerializeField] private float _timer;
+    [SerializeField] private Slider _timerSlider;
+    [SerializeField] private float _minTimer;
+    [SerializeField] private float _timerStep;
+    [SerializeField] private GameObject _failScreen;
+    [SerializeField] private TextMeshProUGUI _failScoreText;
+    [SerializeField] private Button _retryButton;
+    [SerializeField] private Button _mainMenuButton;
 
     private int _index;
     private int _indexL;
     private int _errors;
     private int _score;
+    private Coroutine _timerCoroutine;
 
     private void Start()
     {
         ChangeGame();
         _pauseButton.onClick.AddListener(() => Pause(true));
-        _peachButton.onClick.AddListener(() => Peach());
-        _lavandaButton.onClick.AddListener(() => Lavanda());
-        _yellowButton.onClick.AddListener(() => Yellow());
-        _mintButton.onClick.AddListener(() => Mint());
+        _peachButton.onClick.AddListener(() => ButtonClick(3, 6, 9, 0, 1, 2));
+        _lavandaButton.onClick.AddListener(() => ButtonClick(0, 7, 10, 3, 4, 5));
+        _yellowButton.onClick.AddListener(() => ButtonClick(1, 4, 11, 6, 7, 8));
+        _mintButton.onClick.AddListener(() => ButtonClick(2, 5, 8, 9, 10, 11));
         _continueButton.onClick.AddListener(() => Pause(false));
+        //_leaveGameButton.onClick.AddListener(() => );
+        _retryButton.onClick.AddListener(() => Retry());
+        //_mainMenuButton.onClick.AddListener(() => );
     }
 
     private void ChangeGame()
     {
+        if (_timerCoroutine != null)
+        {
+            StopCoroutine(_timerCoroutine);
+        }
+
         System.Random rand = new System.Random();
         _index = rand.Next(_textSprites.Count);
         _textImage.sprite = _textSprites[_index];
@@ -46,6 +65,24 @@ public class UIManager : MonoBehaviour
         _indexL = randL.Next(_labelsObj.Count);
         _labelsObj.ForEach(obj => obj.SetActive(false));
         _labelsObj[_indexL].SetActive(true);
+
+        _timerSlider.maxValue = _timer;
+        _timerSlider.value = _timer;
+
+        _timerCoroutine = StartCoroutine(Timer());
+    }
+
+    private IEnumerator Timer()
+    {
+        float timeRemaining = _timer;
+        while (timeRemaining > 0)
+        {
+            timeRemaining -= Time.deltaTime;
+            _timerSlider.value = timeRemaining;
+            yield return null;
+        }
+        Error();
+        ChangeGame();
     }
 
     private void Pause(bool pause)
@@ -54,52 +91,15 @@ public class UIManager : MonoBehaviour
         Time.timeScale = pause ? 0 : 1;
     }
 
-    private void Peach()
+    private void ButtonClick(int indexColor1, int indexColor2, int indexColor3,
+        int indexText1, int indexText2, int indexText3)
     {
-        if ((_indexL == 0 && (_index == 3 || _index == 6 || _index == 9)) ||
-            (_indexL == 1 && (_index == 0 || _index == 1 || _index == 2)))
+        if (_timerCoroutine != null)
         {
-            Score();
+            StopCoroutine(_timerCoroutine);
         }
-        else
-        {
-            Error();
-        }
-        ChangeGame();
-    }
-
-    private void Lavanda()
-    {
-        if ((_indexL == 0 && (_index == 0 || _index == 7 || _index == 10)) ||
-            (_indexL == 1 && (_index == 3 || _index == 4 || _index == 5)))
-        {
-            Score();
-        }
-        else
-        {
-            Error();
-        }
-        ChangeGame();
-    }
-
-    private void Yellow()
-    {
-        if ((_indexL == 0 && (_index == 1 || _index == 4 || _index == 11)) ||
-            (_indexL == 1 && (_index == 6 || _index == 7 || _index == 8)))
-        {
-            Score();
-        }
-        else
-        {
-            Error();
-        }
-        ChangeGame();
-    }
-
-    private void Mint()
-    {
-        if ((_indexL == 0 && (_index == 2 || _index == 5 || _index == 8)) ||
-            (_indexL == 1 && (_index == 9 || _index == 10 || _index == 11)))
+        if ((_indexL == 0 && (_index == indexColor1 || _index == indexColor2 || _index == indexColor3)) ||
+            (_indexL == 1 && (_index == indexText1 || _index == indexText2 || _index == indexText3)))
         {
             Score();
         }
@@ -114,11 +114,22 @@ public class UIManager : MonoBehaviour
     {
         _errors++;
         _errorsObj.ForEach(obj => obj.SetActive(_errorsObj.IndexOf(obj) >= _errors));
+        if (_errors == _errorsObj.Count)
+        {
+            _failScreen.SetActive(true);
+            _failScoreText.text = _score.ToString();
+        }
     }
 
     private void Score()
     {
         _score++;
         _scoreText.text = _score.ToString();
+        _timer = Mathf.Max(_minTimer, _timer - _timerStep);
+    }
+
+    private void Retry()
+    {
+        SceneManager.LoadScene("Game");
     }
 }
