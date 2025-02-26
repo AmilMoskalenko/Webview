4926cff67e576ff307d73d9a98d6f4d154ad35a3
AmilMoskalenko <amil.sagyn99@gmail.com>
2025-02-26 15:13:55 +0600

    - Code style, build

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -7,8 +7,13 @@ using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
+    [SerializeField] private float _timer;
+    [SerializeField] private float _minTimer;
+    [SerializeField] private float _timerStep;
+    [Header("Game Screen")]
     [SerializeField] private TextMeshProUGUI _scoreText;
     [SerializeField] private Button _pauseButton;
+    [SerializeField] private Slider _timerSlider;
     [SerializeField] private List<GameObject> _errorsObj;
     [SerializeField] private Image _textImage;
     [SerializeField] private List<Sprite> _textSprites;
@@ -17,13 +22,11 @@ public class UIManager : MonoBehaviour
     [SerializeField] private Button _lavandaButton;
     [SerializeField] private Button _yellowButton;
     [SerializeField] private Button _mintButton;
+    [Header("Pause Screen")]
     [SerializeField] private GameObject _pauseScreen;
     [SerializeField] private Button _continueButton;
     [SerializeField] private Button _leaveGameButton;
-    [SerializeField] private float _timer;
-    [SerializeField] private Slider _timerSlider;
-    [SerializeField] private float _minTimer;
-    [SerializeField] private float _timerStep;
+    [Header("Fail Screen")]
     [SerializeField] private GameObject _failScreen;
     [SerializeField] private TextMeshProUGUI _failScoreText;
     [SerializeField] private Button _retryButton;
@@ -39,10 +42,10 @@ public class UIManager : MonoBehaviour
     {
         ChangeGame();
         _pauseButton.onClick.AddListener(() => Pause(true));
-        _peachButton.onClick.AddListener(() => ButtonClick(3, 6, 9, 0, 1, 2));
-        _lavandaButton.onClick.AddListener(() => ButtonClick(0, 7, 10, 3, 4, 5));
-        _yellowButton.onClick.AddListener(() => ButtonClick(1, 4, 11, 6, 7, 8));
-        _mintButton.onClick.AddListener(() => ButtonClick(2, 5, 8, 9, 10, 11));
+        _peachButton.onClick.AddListener(() => ButtonCheck(3, 6, 9, 0, 1, 2));
+        _lavandaButton.onClick.AddListener(() => ButtonCheck(0, 7, 10, 3, 4, 5));
+        _yellowButton.onClick.AddListener(() => ButtonCheck(1, 4, 11, 6, 7, 8));
+        _mintButton.onClick.AddListener(() => ButtonCheck(2, 5, 8, 9, 10, 11));
         _continueButton.onClick.AddListener(() => Pause(false));
         _leaveGameButton.onClick.AddListener(() => SceneManager.LoadScene("Menu"));
         _retryButton.onClick.AddListener(() => SceneManager.LoadScene("Game"));
@@ -52,23 +55,17 @@ public class UIManager : MonoBehaviour
     private void ChangeGame()
     {
         if (_timerCoroutine != null)
-        {
             StopCoroutine(_timerCoroutine);
-        }
-
         System.Random rand = new System.Random();
         _index = rand.Next(_textSprites.Count);
         _textImage.sprite = _textSprites[_index];
         _textImage.SetNativeSize();
-
         System.Random randL = new System.Random();
         _indexL = randL.Next(_labelsObj.Count);
         _labelsObj.ForEach(obj => obj.SetActive(false));
         _labelsObj[_indexL].SetActive(true);
-
         _timerSlider.maxValue = _timer;
         _timerSlider.value = _timer;
-
         _timerCoroutine = StartCoroutine(Timer());
     }
 
@@ -91,22 +88,20 @@ public class UIManager : MonoBehaviour
         Time.timeScale = pause ? 0 : 1;
     }
 
-    private void ButtonClick(int indexColor1, int indexColor2, int indexColor3,
+    private void ButtonCheck(int indexColor1, int indexColor2, int indexColor3,
         int indexText1, int indexText2, int indexText3)
     {
         if (_timerCoroutine != null)
-        {
             StopCoroutine(_timerCoroutine);
-        }
         if ((_indexL == 0 && (_index == indexColor1 || _index == indexColor2 || _index == indexColor3)) ||
             (_indexL == 1 && (_index == indexText1 || _index == indexText2 || _index == indexText3)))
         {
-            Score();
+            _score++;
+            _scoreText.text = _score.ToString();
+            _timer = Mathf.Max(_minTimer, _timer - _timerStep);
         }
         else
-        {
             Error();
-        }
         ChangeGame();
     }
 
@@ -120,12 +115,5 @@ public class UIManager : MonoBehaviour
             _failScoreText.text = _score.ToString();
             PlayerPrefs.SetInt("Score", _score);
         }
-    }
-
-    private void Score()
-    {
-        _score++;
-        _scoreText.text = _score.ToString();
-        _timer = Mathf.Max(_minTimer, _timer - _timerStep);
     }
 }
